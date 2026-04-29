6cc5480cafcd5030aab04865f2220e2225b04732
Valentino <valentinomendez11@gmail.com>
2026-04-29 18:13:08 -0300

    Primeros ejercicios de lógica con bucles for y while

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import javax.sound.midi.Soundbank;
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 1) imprimir los números del 1 al 10.
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(i);
+//        }
+
+
+        // 2) imprimir los numeros pares del 0 al 50.
+//        for (int i = 0; i < 51; i++) {
+//            if(i%2==0)
+//                System.out.println(i);
+//        }
+
+        // 3) Sumar los numeros del 1 al 100 e imprimir el resultado.
+//        int suma = 0;
+//        for (int i = 1; i < 101; i++) {
+//            suma+=i;
+//        }
+//        System.out.println(suma);
+
+
+        //WHILE:
+        // 1) imprimir en cuenta regresiva de 5 hasta 1 y ¡Despuegue!
+//        int cuenta=5;
+//        while(cuenta>0){
+//            System.out.println(cuenta);
+//            cuenta--;
+//        }
+//        System.out.println("¡Despuegue!");
+
+
+        // 2) Validación de contraseña: pedir al usuario una contraseña y verificar que sea correcta.
+//        String contraseñaCorrecta="0123";
+//        System.out.print("Ingrese su contraseña: ");
+//        String contraseñaUsuario=sc.nextLine();
+//        while(!contraseñaUsuario.equals(contraseñaCorrecta)){
+//            System.out.println("Error: contraseña incorrecta.");
+//            System.out.print("Ingrese nuevamente su contraseña: ");
+//            contraseñaUsuario=sc.nextLine();
+//        }
+//        System.out.println("Contraseña correcta!");
+
+
+        //3) adivinar un número: detener el ciclo cuando el numero ingresado sea igual al condicional.
+//        int numeroCorrecto=5;
+//        System.out.print("Ingrese un número: ");
+//        int numeroUsuario=sc.nextInt();
+//        while(numeroUsuario!=numeroCorrecto){
+//            System.out.print("Incorrecto, vuelva a ingresar el número: ");
+//            numeroUsuario=sc.nextInt();
+//        }
+//        System.out.println("Correcto, haz acertado el número!");
+
+    }
+}
